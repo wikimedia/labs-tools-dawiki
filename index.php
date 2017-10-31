54ea534edc76456ce35bedf0044eb6b3d03de0fc
Simeon Dahl <simeondahl@gmail.com>
2017-10-31 18:20:25 +0100

    Added files and style
    Added bootstrap and then some edit on the front page.
    
    Change-Id: I9e042d121442493e650f243b835de23e19c91f35

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Hello!
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>DA.WIKI Tools</title>
+
+    <!-- META -->
+    <meta charset="UTF-8">
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- JS -->
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h4>Danske Wiki Værktøjer</h4>
+    Velkommen til danske Wikipedia værktøjer.<br>
+    <i>der er desværre ikke nogle værktøjer lige pt.</i>
+</div>
+</body>
+</html>
